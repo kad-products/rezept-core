648dd5b23ba3606127e0955dbc3e04bf38111008
Adam Dehnel <arsdehnel@gmail.com>
2026-02-15 14:08:09 -0600

    chore: wire up text coverage and thresholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,35 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'node',
+		coverage: {
+			provider: 'v8', // or 'istanbul'
+			reporter: ['text', 'json-summary'],
+			include: ['src/actions/**', 'src/repositories/**', 'src/schemas/**', 'src/session/**', 'src/utils/**'],
+			exclude: [
+				'**/*.md',
+				'**/__tests__/**',
+				'**/*.test.ts',
+				'**/*.integration.test.ts',
+				'src/components/**',
+				'src/pages/**',
+				'src/layouts/**',
+				'src/styles/**',
+				'src/session/store.ts', // Just config
+			],
+			thresholds: {
+				branches: 23,
+				lines: 30, // using this to make sure we don't miss something big or have dead code
+				'src/actions/**': {
+					branches: 8,
+				},
+				'src/repositories/**': {
+					branches: 40,
+				},
+				'src/utils/**': {
+					branches: 0,
+				},
+			},
+		},
 		alias: {
 			'cloudflare:workers': path.resolve(__dirname, 'tests/mocks/cloudflare-workers.ts'),
 			'rwsdk/auth': path.resolve(__dirname, 'tests/mocks/rwsdk-auth.ts'),
