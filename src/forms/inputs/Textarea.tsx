4c9faaf02c8564bcaf4a75eadbff4f8ed660ec26
Adam Dehnel <arsdehnel@gmail.com>
2026-02-17 17:56:36 -0600

    feat: add form context for the TanStack implementation
    This isn't used yet but trying to keep these changes/commits smaller
    so this gets us the starting point.

---
+++ src/forms/inputs/Textarea.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { useFieldContext } from '../context';
+
+export function TextareaInput({ label, required = false }: { label: string; required?: boolean }) {
+	const field = useFieldContext<string>();
+	return (
+		<div className="form-field">
+			<div className="form-inputs">
+				<label htmlFor={field.name}>
+					{label}
+					{required && <span className="required">*</span>}
+				</label>
+				<textarea
+					id={field.name}
+					name={field.name}
+					value={field.state.value}
+					required={required}
+					onChange={e => field.handleChange(e.target.value)}
+				/>
+				{!field.state.meta.isValid && (
+					<div className="form-field-error">
+						{field.state.meta.errors.map(error => (
+							<p key={error} className="error">
+								{error}
+							</p>
+						))}
+					</div>
+				)}
+			</div>
+		</div>
+	);
+}
---
+++ src/forms/inputs/index.ts
@@ -0,0 +1,4 @@
+export * from './CheckboxGroup';
+export * from './Select';
+export * from './Text';
+export * from './Textarea';
