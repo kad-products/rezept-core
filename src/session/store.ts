57d9a074ee5a3890b9a077b13c49381f344edb49
Adam Dehnel <arsdehnel@gmail.com>
2026-03-14 16:23:34 -0500

    fix: remove all the bookmarklet stuff

--- src/session/store.ts
+++ src/session/store.ts
@@ -1,14 +1,7 @@
 import { env } from 'cloudflare:workers';
-import { type createSessionCookie, defineDurableSession, MAX_SESSION_DURATION } from 'rwsdk/auth';
-
-const createCookie: typeof createSessionCookie = ({ name, sessionId, maxAge }) => {
-	return `${name}=${sessionId}; Path=/; HttpOnly; Secure; SameSite=None${
-		maxAge != null ? `; Max-Age=${maxAge === true ? MAX_SESSION_DURATION / 1000 : maxAge}` : ''
-	}`;
-};
+import { defineDurableSession } from 'rwsdk/auth';
 
 export const sessions = defineDurableSession({
 	secretKey: env.SESSION_SECRET_KEY,
 	sessionDurableObject: env.SESSION_DURABLE_OBJECT,
-	createCookie,
 });
