0e013eed79f99cf6ee3390e520d53842fa448ee2
Adam Dehnel <arsdehnel@gmail.com>
2026-03-14 13:19:14 -0500

    feat: custom cookie to enable bookmarklet calls

--- src/session/store.ts
+++ src/session/store.ts
@@ -1,7 +1,14 @@
 import { env } from 'cloudflare:workers';
-import { defineDurableSession } from 'rwsdk/auth';
+import { type createSessionCookie, defineDurableSession, MAX_SESSION_DURATION } from 'rwsdk/auth';
+
+const createCookie: typeof createSessionCookie = ({ name, sessionId, maxAge }) => {
+	return `${name}=${sessionId}; Path=/; HttpOnly; Secure; SameSite=None${
+		maxAge != null ? `; Max-Age=${maxAge === true ? MAX_SESSION_DURATION / 1000 : maxAge}` : ''
+	}`;
+};
 
 export const sessions = defineDurableSession({
 	secretKey: env.SESSION_SECRET_KEY,
 	sessionDurableObject: env.SESSION_DURABLE_OBJECT,
+	createCookie,
 });
