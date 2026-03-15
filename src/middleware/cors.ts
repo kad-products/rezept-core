b26370431f4e19f7b95e1211a40892967fa8809e
Adam Dehnel <arsdehnel@gmail.com>
2026-03-15 13:40:59 -0500

    feat: adding cors middleware

---
+++ src/middleware/cors.ts
@@ -0,0 +1,23 @@
+import type { DefaultAppContext, RequestInfo } from 'rwsdk/worker';
+
+const allowedCorsPaths: Record<string, string[]> = {
+	OPTIONS: ['/api/recipes/import/bookmarklet'],
+};
+
+export default function corsMiddleware({ request }: RequestInfo<DefaultAppContext>) {
+	const allowedPaths = allowedCorsPaths[request.method];
+
+	if (allowedPaths) {
+		if (allowedPaths.includes(new URL(request.url).pathname)) {
+			return new Response(null, {
+				status: 204,
+				headers: {
+					'Access-Control-Allow-Origin': request.headers.get('Origin') ?? '*',
+					'Access-Control-Allow-Credentials': 'true',
+					'Access-Control-Allow-Methods': 'POST, OPTIONS',
+					'Access-Control-Allow-Headers': 'Content-Type',
+				},
+			});
+		}
+	}
+}
