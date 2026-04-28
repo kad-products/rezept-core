1ddbe7e15d8c6e056e236ceb597f35df62c55915
Adam Dehnel <arsdehnel@gmail.com>
2026-04-27 19:50:10 -0500

    fix: swap out react-select for radix select

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+	overrides: [
+		{
+			files: ['*.less', '*.css'],
+			options: {
+				printWidth: 100,
+				tabWidth: 4,
+				useTabs: true,
+			},
+		},
+	],
+};
--- src/forms/inputs/Select.tsx
+++ src/forms/inputs/Select.tsx
@@ -1,5 +1,6 @@
 'use client';
-import ReactSelect from 'react-select';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+import { Form, Select as RadixSelect } from 'radix-ui';
 import { useFieldContext } from '../context';
 
 interface SelectProps<T = string> {
@@ -12,30 +13,44 @@ export function Select<T = string>({ label, options, required }: SelectProps<T>)
 	const field = useFieldContext<T>();
 
 	return (
-		<div className="form-field">
-			<div className="form-inputs">
-				<label htmlFor={field.name}>
-					{label}
-					{required && <span className="required">*</span>}
-				</label>
-				<ReactSelect
-					instanceId={`${field.name}-select`}
-					name={field.name}
-					options={options}
-					value={options.find(opt => opt.value === field.state.value)}
-					onChange={selected => field.handleChange(selected?.value as T)}
-					onBlur={field.handleBlur}
-				/>
-			</div>
+		<Form.Field className="rz-form-field" name={field.name}>
+			<Form.Label className="rz-form-label">
+				{label}
+				{required && <span className="rz-form-input-required">*</span>}
+			</Form.Label>
 			{!field.state.meta.isValid && (
-				<div className="form-field-error">
+				<div className="rz-form-field-error">
 					{field.state.meta.errors.map(error => (
-						<p key={error.code} className="error">
+						<Form.Message key={error.code} className="rz-form-message" forceMatch={true}>
 							{error.message}
-						</p>
+						</Form.Message>
 					))}
 				</div>
 			)}
-		</div>
+			<Form.Control asChild>
+				<RadixSelect.Root onValueChange={value => field.handleChange(value as T)} value={field.state.value as unknown as string}>
+					<RadixSelect.Trigger className="rz-select-trigger">
+						<RadixSelect.Value placeholder="Select an option" />
+						<RadixSelect.Icon className="rz-select-icon">
+							<ChevronDownIcon />
+						</RadixSelect.Icon>
+					</RadixSelect.Trigger>
+					<RadixSelect.Content className="rz-select-content">
+						<RadixSelect.Viewport className="rz-select-viewport">
+							{options.map(option => (
+								<RadixSelect.Item
+									key={option.value as unknown as string}
+									value={option.value as unknown as string}
+									className="rz-select-item"
+								>
+									<RadixSelect.ItemText>{option.label}</RadixSelect.ItemText>
+									<RadixSelect.ItemIndicator className="rz-select-item-indicator" />
+								</RadixSelect.Item>
+							))}
+						</RadixSelect.Viewport>
+					</RadixSelect.Content>
+				</RadixSelect.Root>
+			</Form.Control>
+		</Form.Field>
 	);
 }
