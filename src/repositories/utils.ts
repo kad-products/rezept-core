961a909cdca92c977c33f83b31a6be815c4121ef
Adam Dehnel <arsdehnel@gmail.com>
2026-04-28 19:38:06 -0500

    fix: type for repository utils

--- src/repositories/utils.ts
+++ src/repositories/utils.ts
@@ -1,5 +1,5 @@
 const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
-export function validateUuid(uuid: string) {
+export function validateUuid(uuid: string): boolean {
 	return UUID_REGEX.test(uuid);
 }
