e717b8ac70e1da9f663328d352bab6ce33cf18f4
Adam Dehnel <arsdehnel@gmail.com>
2026-05-01 22:54:05 -0500

    fix: adjust middleware return pattern
    Closes https://github.com/kad-products/rezept-core/issues/121

--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -2,25 +2,23 @@ import type { DefaultAppContext, RequestInfo } from 'rwsdk/worker';
 import { sessions } from '@/durable-objects/store';
 import { getUserById } from '@/repositories';
 
-export default async function userMiddleware({ ctx, request }: RequestInfo<DefaultAppContext>): Promise<Response | undefined> {
+export default async function userMiddleware({
+	ctx,
+	request,
+	response,
+}: RequestInfo<DefaultAppContext>): Promise<Response | undefined> {
 	if (ctx.session?.userId) {
 		try {
 			ctx.user = await getUserById(ctx.session.userId, ctx.logger);
 		} catch (err) {
 			ctx.logger.error(`Error fetching current user: ${err}`);
-			const headers = new Headers();
-			await sessions.remove(request, headers);
-			headers.set('Location', '/');
+			await sessions.remove(request, response.headers);
+			response.headers.set('Location', '/');
 
-			return Response.json(
-				{
-					message: 'User found on session but could not be retrieved',
-				},
-				{
-					status: 302,
-					headers,
-				},
-			);
+			return Response.json(null, {
+				status: 302,
+				headers: response.headers,
+			});
 		}
 	}
 }
