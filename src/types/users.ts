a8642bdb79955a036d2f36750c567437e3a52cea
Adam Dehnel <arsdehnel@gmail.com>
2026-05-03 18:15:03 -0500

    refactor: remove unused UserFormInput type

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,10 +1,7 @@
-import type { z } from 'zod';
 import type { users } from '@/models';
-import type { usersSchemas } from '@/schemas';
 
 export type UserDBRead = typeof users.$inferSelect;
 export type UserWriteInput = Omit<
 	typeof users.$inferInsert,
 	'id' | 'createdAt' | 'createdBy' | 'updatedAt' | 'updatedBy' | 'deletedAt' | 'deletedBy'
 >;
-export type UserFormData = z.input<typeof usersSchemas.form>;
