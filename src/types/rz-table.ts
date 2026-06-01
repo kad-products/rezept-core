edccd2075e71afead7dbf853e7eeff8dc7a89750
Adam Dehnel <arsdehnel@gmail.com>
2026-06-01 08:37:47 -0500

    feat: add action link option to RzTable

--- src/types/rz-table.ts
+++ src/types/rz-table.ts
@@ -3,5 +3,10 @@ import type { JSX } from 'react';
 export type RzTableColumn = {
 	label: string;
 	key: string;
+	action?: {
+		type: 'link' | 'button';
+		hrefProp?: string;
+		label: string;
+	};
 	render?: (val: string, record: Record<string, unknown>) => JSX.Element | string;
 };
