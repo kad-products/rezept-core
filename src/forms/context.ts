c30311c47770ab25e620d84286433da7e56e55e0
Adam Dehnel <arsdehnel@gmail.com>
2026-06-30 10:59:23 -0500

    feat: add date input to form components

--- src/forms/context.ts
+++ src/forms/context.ts
@@ -1,12 +1,13 @@
 import { createFormHook, createFormHookContexts } from '@tanstack/react-form';
 
-import { CheckboxGroup, NumberInput, Select, Submit, TextareaInput, TextInput } from './inputs';
+import { CheckboxGroup, DateInput, NumberInput, Select, Submit, TextareaInput, TextInput } from './inputs';
 
 export const { fieldContext, formContext, useFieldContext, useFormContext } = createFormHookContexts();
 
 export const { useAppForm } = createFormHook({
 	fieldComponents: {
 		CheckboxGroup,
+		DateInput,
 		NumberInput,
 		Select,
 		TextInput,
