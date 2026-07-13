2185791c094eb303a7c2cb0c74fb384097e7a3bb
Adam Dehnel <arsdehnel@gmail.com>
2026-07-13 14:40:19 -0500

    fix: just alter the users table rather than drop/create

--- drizzle/0028_wild_fixer.sql
+++ drizzle/0028_wild_fixer.sql
@@ -1,23 +1,6 @@
 DROP INDEX `ingredient_seasons_ingredient_id_country_region_unique`;--> statement-breakpoint
 CREATE UNIQUE INDEX `ingredient_seasons_ingredient_id_country_region_unique` ON `ingredient_seasons` (`ingredient_id`,`country`,`region`) WHERE "deleted_at" IS NULL;--> statement-breakpoint
-PRAGMA defer_foreign_keys = on;--> statement-breakpoint
-CREATE TABLE `__new_users` (
-	`id` text PRIMARY KEY NOT NULL,
-	`username` text NOT NULL,
-	`role` text DEFAULT 'BASIC' NOT NULL,
-	`created_at` text DEFAULT (datetime('now', 'localtime')) NOT NULL,
-	`created_by` text,
-	`updated_at` text,
-	`updated_by` text,
-	`deleted_at` text,
-	`deleted_by` text,
-	FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`updated_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`deleted_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-INSERT INTO `__new_users`("id", "username", "role", "created_at", "created_by", "updated_at", "updated_by", "deleted_at", "deleted_by") SELECT "id", "username", COALESCE("role",'BASIC'), "created_at", "created_by", "updated_at", "updated_by", "deleted_at", "deleted_by" FROM `users`;--> statement-breakpoint
-DROP TABLE `users`;--> statement-breakpoint
-ALTER TABLE `__new_users` RENAME TO `users`;--> statement-breakpoint
-PRAGMA defer_foreign_keys=OFF;--> statement-breakpoint
-CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);
+ALTER TABLE `users` ADD COLUMN `new_role` text NOT NULL DEFAULT 'BASIC';--> statement-breakpoint
+UPDATE `users` SET `new_role` = `role` WHERE `role` IS NOT NULL;--> statement-breakpoint
+ALTER TABLE `users` DROP COLUMN `role`;--> statement-breakpoint
+ALTER TABLE `users` RENAME COLUMN `new_role` TO `role`;
