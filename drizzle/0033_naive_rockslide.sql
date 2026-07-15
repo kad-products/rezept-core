bb0cc64baa505adcac8e35760b21982cc791915c
Adam Dehnel <arsdehnel@gmail.com>
2026-07-15 16:06:30 -0500

    fix: empty the ingredient seasons table before moving to growing zones

--- drizzle/0033_naive_rockslide.sql
+++ drizzle/0033_naive_rockslide.sql
@@ -2,6 +2,8 @@ DROP INDEX `ingredient_seasons_country_idx`;--> statement-breakpoint
 DROP INDEX `ingredient_seasons_region_idx`;--> statement-breakpoint
 DROP INDEX `ingredient_seasons_country_region_idx`;--> statement-breakpoint
 DROP INDEX `ingredient_seasons_ingredient_id_country_region_unique`;--> statement-breakpoint
+-- manually added this as all existing records are invalid as we move to growing zones
+DELETE FROM `ingredient_seasons`;--> statement-breakpoint
 ALTER TABLE `ingredient_seasons` ADD `growing_zone_id` text NOT NULL REFERENCES growing_zones(id);--> statement-breakpoint
 CREATE INDEX `ingredient_seasons_growing_zone_id_idx` ON `ingredient_seasons` (`growing_zone_id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `ingredient_seasons_ingredient_id_growing_zone_id_unique` ON `ingredient_seasons` (`ingredient_id`,`growing_zone_id`) WHERE "deleted_at" IS NULL;--> statement-breakpoint
